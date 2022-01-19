f4e21316255211fa873e5b885a319bd73d49b653
jack <jackzhoumine@gmail.com>
2022-01-20 02:35:15 +0800

    部署代码

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   resolve: {
     extensions: ['.js', '.jsx', '.json'],
@@ -17,4 +18,9 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
 }
